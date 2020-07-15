c72104551ec8fceb5d956815af754141fcbfa5fe
tevoran <j.pfeiffer93@gmx.de>
2020-07-15 21:21:23 +0200

    rmoved small mistake within directory

--- src/include/wce/wce_planet_properties.c
+++ src/include/wce/wce_planet_properties.c
@@ -216,24 +216,3 @@ struct planet wce_create_planet(long seed)
         return world;
         
 }
-
-//randomgenerator (after Blum-Blum-Shub)
-//seed has max. 9 decimals
-//the output has also max 9 decimals
-long wce_random(long seed)
-{
-  long p=10007;
-  long q=100003;
-  long n=p*q;
-  long output;
-  output=seed;
-  while(1)
-    {
-      output=(output*output)%n;
-        if(output<1000000000)
-        {
-            break;
-        }
-    }  
-  return output;
-}
